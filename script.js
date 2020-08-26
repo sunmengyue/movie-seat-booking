8adc5e7fc16978eb81ea58b5bf0c6fa8158d4e69
Simon <bardrabbit@gmail.com>
2020-08-26 14:05:15 -0400

    populate the local storage to UI

--- script.js
+++ script.js
@@ -5,6 +5,8 @@ const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
 let ticketPrice = +movieSelect.value;
 
+populateUI();
+
 // Save selected movie index and price
 function setMovieData(movieIndex, moviePrice) {
   localStorage.setItem('selectedMovieIndex', movieIndex);
@@ -19,11 +21,26 @@ function updateSelectedCount() {
     [...availableSeats].indexOf(seat)
   );
 
-  localStorage.setItem('seatsNumber', JSON.stringify(seatsIndex));
+  localStorage.setItem('selectedSeats', JSON.stringify(seatsIndex));
 
   const selectedSeatsCount = selectedSeats.length;
   count.innerText = selectedSeatsCount;
   total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+// Get Data from local storage and populate  UI
+function populateUI() {
+  const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+  if (selectedSeats !== null && selectedSeats.length > 0) {
+    availableSeats.forEach((seat, index) => {
+      if (selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected');
+      }
+    });
+  }
+
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+  movieSelect.selectedIndex = selectedMovieIndex;
 }
 
 // Movie select event
@@ -44,3 +61,6 @@ container.addEventListener('click', (e) => {
 
   updateSelectedCount();
 });
+
+// Initial count and total set
+updateSelectedCount();
